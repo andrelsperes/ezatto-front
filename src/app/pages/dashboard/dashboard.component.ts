3f477599c7eba5cbf25faaa6ad64fdb2547db798
André Peres <andre@ezsolucoes.com.br>
2021-06-14 14:30:21 -0300

    ajustes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -23,7 +23,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   view: any[] = [1000, 400];
 
-   
+
     showXAxis = true;
     showYAxis = true;
     gradient = true;
@@ -32,10 +32,10 @@ export class DashboardComponent implements OnInit, OnDestroy {
     showYAxisLabel = true;
     quantidades = 'Quantidades';
     dia = 'Dias';
-    mes = 'Mês';    
+    mes = 'Mês';
     mesQdta = 'Quantidades';
     timeline = true;
-  
+
     colorScheme = {
       domain: ColorSchema.picnic,
     };
@@ -43,11 +43,11 @@ export class DashboardComponent implements OnInit, OnDestroy {
   public autuacaoDiasRowData: Array<any> = [];
   public autuacaoMesRowData: Array<any> = [];
   public motivoInfracaoRowData: Array<any> = [];
- 
+
   constructor(
     private sidebarService: NbSidebarService,
     private layoutService: LayoutService,
-    private commonService: CommonService,   
+    private commonService: CommonService,
     private formBuilder: FormBuilder,
 
   ) {
@@ -55,23 +55,23 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.motivoInfracaoRowData = [
 
       {
-        name: 'Infração 1',
+        name: 'Produto 1',
         value: '25.0',
       },
       {
-        name: 'Infração 2',
+        name: 'Produto 2',
         value: '25.0',
       },
       {
-        name: 'Infração 3',
+        name: 'Produto 3',
         value: '25.0',
       },
       {
-        name: 'Infração 4',
+        name: 'Produto 4',
         value: '25.0',
       },
-       
-          
+
+
     ]
 
     this.autuacaoMesRowData = [
@@ -129,7 +129,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
     ]
 
     this.autuacaoDiasRowData= [
-      
+
       {
         name: '01',
         value: '2',
@@ -137,130 +137,130 @@ export class DashboardComponent implements OnInit, OnDestroy {
       {
         name: '02',
         value: '15.0',
-      },   
+      },
       {
         name: '03',
         value: '25.0',
-      },  
+      },
       {
         name: '04',
         value: '5.0',
-      },  
+      },
       {
         name: '05',
         value: '35.0',
-      },  
+      },
       {
         name: '06',
         value: '10.0',
-      },  
+      },
       {
         name: '07',
         value: '1.0',
-      }, 
+      },
       {
         name: '08',
         value: '7.0',
-      },  
+      },
       {
         name: '09',
         value: '9.0',
-      },  
+      },
       {
         name: '10',
         value: '11.0',
-      },  
+      },
       {
         name: '11',
         value: '18.0',
-      },  
+      },
       {
         name: '12',
         value: '1.0',
-      },  
+      },
       {
         name: '13',
         value: '16.0',
-      },  
+      },
       {
         name: '14',
         value: '1.0',
-      },  
+      },
       {
         name: '15',
         value: '20.0',
-      },  
+      },
       {
         name: '16',
         value: '21.0',
-      },  
+      },
       {
         name: '17',
         value: '22.0',
-      },  
+      },
       {
         name: '18',
         value: '18.0',
-      },  
+      },
       {
         name: '19',
         value: '6.0',
-      },  
+      },
       {
         name: '20',
         value: '7.0',
-      },   
+      },
       {
         name: '21',
         value: '9.0',
-      },  
+      },
       {
         name: '22',
         value: '6.0',
-      },  
+      },
       {
         name: '23',
         value: '5.0',
-      },  
+      },
       {
         name: '24',
         value: '4.0',
-      },  
+      },
       {
         name: '25',
         value: '9.0',
-      },  
+      },
       {
         name: '26',
         value: '3.0',
-      },  
+      },
       {
         name: '27',
         value: '15.0',
-      },  
+      },
       {
         name: '28',
         value: '19.0',
-      }, 
+      },
       {
         name: '29',
         value: '15.0',
-      },  
+      },
       {
         name: '30',
         value: '2.0',
-      }          
+      }
 
-    ]   
-    
+    ]
+
   }
 
-  ngOnInit() {   
+  ngOnInit() {
 
   }
 
   ngOnDestroy() {
   }
-  
+
 
 }
